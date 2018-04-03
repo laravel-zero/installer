08c57f981568067caecffbfdafe73f2b446a4d3b
Nuno Maduro <enunomaduro@gmail.com>
2018-04-03 22:57:48 +0200

    Updates build file

--- config/app.php
+++ config/app.php
@@ -24,7 +24,7 @@ return [
     | number MAJOR.MINOR.PATCH when an update happens: https://semver.org.
     |
     */
-    'version' => 'v2.0.3',
+    'version' => 'v2.0.4',
 
     /*
     |--------------------------------------------------------------------------
